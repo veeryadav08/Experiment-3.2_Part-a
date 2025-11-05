31a30eaee91345bd673bc3d85fa66d85ebcc72ec
veeryadav08 <veermehta4321@gmail.com>
2025-11-05 18:28:36 +0530

    MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,12 @@
+package com.example.springdi;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        student.displayInfo();
+    }
+}
